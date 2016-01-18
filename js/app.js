09e8467d09c09d9e40143995a15252c61dcba005
bharding2 <b.harding@gmail.com>
2016-01-18 15:01:18 -0800

    unhiding button

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var allProducts = [];
+var totalClicks = 0;
 
 function Product(productName, filePath) {
   this.productName = productName;
@@ -27,8 +28,6 @@ var usb = new Product('usb', 'img/usb.jpg');
 var watercan = new Product('watercan', 'img/watercan.jpg');
 var wineglass = new Product('wineglass', 'img/wineglass.jpg');
 
-
-//Random number 0 - allProducts.length -1
 function randomProduct() {
   return Math.floor(Math.random() * allProducts.length);
 }
@@ -36,33 +35,75 @@ function randomProduct() {
 var productOneIndex = 0;
 var productTwoIndex = 0;
 var productThreeIndex = 0;
+var imgOne = document.getElementById('imgOne');
+var imgTwo = document.getElementById('imgTwo');
+var imgThree = document.getElementById('imgThree');
 
 function displayThree() {
   productOneIndex = randomProduct();
-  var imgOne = document.getElementById('imgOne');
   imgOne.setAttribute('src', allProducts[productOneIndex].filePath);
+  allProducts[productOneIndex].numDisplays += 1;
 
   productTwoIndex = randomProduct();
   while (productTwoIndex === productOneIndex)  {
     productTwoIndex = randomProduct();
   }
-  var imgTwo = document.getElementById('imgTwo');
   imgTwo.setAttribute('src', allProducts[productTwoIndex].filePath);
+  allProducts[productTwoIndex].numDisplays += 1;
 
   productThreeIndex = randomProduct();
   while (productThreeIndex === productOneIndex || productThreeIndex === productTwoIndex) {
     productThreeIndex = randomProduct();
   }
-  var imgThree = document.getElementById('imgThree');
   imgThree.setAttribute('src', allProducts[productThreeIndex].filePath);
+  allProducts[productThreeIndex].numDisplays += 1;
 
   console.log(productOneIndex);
   console.log(productTwoIndex);
   console.log(productThreeIndex);
 }
-//display 3 unique random images
-//increment times displayed
+displayThree();
 
-//event handling for user clicks
-//increment vote on the one that was clicked
-//after 15 times, give the option to show results
+var resultButton = document.getElementById('showResults');
+
+imgOne.addEventListener('click', handleImgOneClick);
+
+function handleImgOneClick(event) {
+  allProducts[productOneIndex].numClicks += 1;
+  totalClicks += 1;
+  if (totalClicks === 15) {
+    resultButton.removeAttribute('hidden');
+  }
+
+  console.log(allProducts[productOneIndex].productName + ' clicked ' + allProducts[productOneIndex].numClicks + ' times');
+
+  displayThree();
+}
+
+imgTwo.addEventListener('click', handleImgTwoClick);
+
+function handleImgTwoClick(event) {
+  allProducts[productTwoIndex].numClicks += 1;
+  totalClicks += 1;
+  if (totalClicks === 15) {
+    resultButton.removeAttribute('hidden');
+  }
+
+  console.log(allProducts[productTwoIndex].productName + ' clicked ' + allProducts[productTwoIndex].numClicks + ' times');
+
+  displayThree();
+}
+
+imgThree.addEventListener('click', handleImgThreeClick);
+
+function handleImgThreeClick(event) {
+  allProducts[productThreeIndex].numClicks += 1;
+  totalClicks += 1;
+  if (totalClicks === 15) {
+    resultButton.removeAttribute('hidden');
+  }
+
+  console.log(allProducts[productThreeIndex].productName + ' clicked ' + allProducts[productThreeIndex].numClicks + ' times');
+
+  displayThree();
+}
