b3cd86282f72c945817e87248b5c054dbe61ef7c
bharding2 <b.harding@gmail.com>
2016-01-19 19:21:44 -0800

    major image handling refactor using arrays and event.path.id

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var allProducts = [];
 var totalClicks = 0;
 var originalIndex = 0;
 
@@ -10,55 +9,50 @@ function Product(productName, filePath) {
   this.numClicks = 0;
   this.numDisplays = 0;
   this.percentClicked = 0;
-  this.originalIndex = originalIndex;
-  originalIndex += 1;
-
-  allProducts.push(this);
+  this.originalIndex = originalIndex++;
 }
 
-var bag = new Product('bag', 'img/bag.jpg');
-var banana = new Product('banana', 'img/banana.jpg');
-var boots = new Product('boots', 'img/boots.jpg');
-var chair = new Product('chair', 'img/chair.jpg');
-var cthulhu = new Product('cthulhu', 'img/cthulhu.jpg');
-var dragon = new Product('dragon', 'img/dragon.jpg');
-var pen = new Product('pen', 'img/pen.jpg');
-var scissors = new Product('scissors', 'img/scissors.jpg');
-var shark = new Product('shark', 'img/shark.jpg');
-var sweep = new Product('sweep', 'img/sweep.jpg');
-var unicorn = new Product('unicorn', 'img/unicorn.jpg');
-var usb = new Product('usb', 'img/usb.jpg');
-var watercan = new Product('watercan', 'img/watercan.jpg');
-var wineglass = new Product('wineglass', 'img/wineglass.jpg');
+var allProducts = [
+  new Product('bag', 'img/bag.jpg'),
+  new Product('banana', 'img/banana.jpg'),
+  new Product('boots', 'img/boots.jpg'),
+  new Product('chair', 'img/chair.jpg'),
+  new Product('cthulhu', 'img/cthulhu.jpg'),
+  new Product('dragon', 'img/dragon.jpg'),
+  new Product('pen', 'img/pen.jpg'),
+  new Product('scissors', 'img/scissors.jpg'),
+  new Product('shark', 'img/shark.jpg'),
+  new Product('sweep', 'img/sweep.jpg'),
+  new Product('unicorn', 'img/unicorn.jpg'),
+  new Product('usb', 'img/usb.jpg'),
+  new Product('watercan', 'img/watercan.jpg'),
+  new Product('wineglass', 'img/wineglass.jpg')
+];
 
-// make not global??? at least an array (would help dry out event handling)
-
-var productOneIndex = 0;
-var productTwoIndex = 0;
-var productThreeIndex = 0;
-var imgOne = document.getElementById('imgOne');
-var imgTwo = document.getElementById('imgTwo');
-var imgThree = document.getElementById('imgThree');
+var imageIndex = [];
+var imgOne = document.getElementById('0');
+var imgTwo = document.getElementById('1');
+var imgThree = document.getElementById('2');
 
 function displayThree() {
-  productOneIndex = Math.floor(Math.random() * allProducts.length);
-  imgOne.setAttribute('src', allProducts[productOneIndex].filePath);
+  imageIndex[0] = Math.floor(Math.random() * allProducts.length);
+  imgOne.setAttribute('src', allProducts[imageIndex[0]].filePath);
 
-  productTwoIndex = Math.floor(Math.random() * allProducts.length);
-  while (productTwoIndex === productOneIndex)  {
-    productTwoIndex = Math.floor(Math.random() * allProducts.length);
+  imageIndex[1] = Math.floor(Math.random() * allProducts.length);
+  while (imageIndex[1] === imageIndex[0])  {
+    imageIndex[1] = Math.floor(Math.random() * allProducts.length);
   }
-  imgTwo.setAttribute('src', allProducts[productTwoIndex].filePath);
+  imgTwo.setAttribute('src', allProducts[imageIndex[1]].filePath);
 
-  productThreeIndex = Math.floor(Math.random() * allProducts.length);
-  while (productThreeIndex === productOneIndex || productThreeIndex === productTwoIndex) {
-    productThreeIndex = Math.floor(Math.random() * allProducts.length);
+  imageIndex[2] = Math.floor(Math.random() * allProducts.length);
+  while (imageIndex[2] === imageIndex[0] || imageIndex[2] === imageIndex[1]) {
+    imageIndex[2] = Math.floor(Math.random() * allProducts.length);
   }
-  imgThree.setAttribute('src', allProducts[productThreeIndex].filePath);
+  imgThree.setAttribute('src', allProducts[imageIndex[2]].filePath);
 
-  console.log(productOneIndex);
-  console.log(productTwoIndex);
-  console.log(productThreeIndex);
+  console.log(imageIndex[0]);
+  console.log(imageIndex[1]);
+  console.log(imageIndex[2]);
 }
 displayThree();
 
@@ -73,59 +67,22 @@ displayThree();
 //
 // or just force the first five passes
 
-// dry these guys out.  array of img with properties of one, two, three?
+imgOne.addEventListener('click', handleImgClick);
+imgTwo.addEventListener('click', handleImgClick);
+imgThree.addEventListener('click', handleImgClick);
 
-imgOne.addEventListener('click', handleImgOneClick);
-
-function handleImgOneClick(event) {
-  allProducts[productOneIndex].numClicks += 1;
-  allProducts[productOneIndex].numDisplays += 1;
-  allProducts[productTwoIndex].numDisplays += 1;
-  allProducts[productThreeIndex].numDisplays += 1;
+function handleImgClick(event) {
+  allProducts[imageIndex[+event.path[0].id]].numClicks += 1;
+  allProducts[imageIndex[0]].numDisplays += 1;
+  allProducts[imageIndex[1]].numDisplays += 1;
+  allProducts[imageIndex[2]].numDisplays += 1;
   totalClicks += 1;
   if (totalClicks === 15) {
     resultButton.removeAttribute('hidden');
   }
-
-  console.log(allProducts[productOneIndex].productName + ' clicked ' + allProducts[productOneIndex].numClicks + ' times');
-
   displayThree();
 }
-
-imgTwo.addEventListener('click', handleImgTwoClick);
-
-function handleImgTwoClick(event) {
-  allProducts[productTwoIndex].numClicks += 1;
-  allProducts[productOneIndex].numDisplays += 1;
-  allProducts[productTwoIndex].numDisplays += 1;
-  allProducts[productThreeIndex].numDisplays += 1;
-  totalClicks += 1;
-  if (totalClicks === 15) {
-    resultButton.removeAttribute('hidden');
-  }
-
-  console.log(allProducts[productTwoIndex].productName + ' clicked ' + allProducts[productTwoIndex].numClicks + ' times');
-
-  displayThree();
-}
-
-imgThree.addEventListener('click', handleImgThreeClick);
-
-function handleImgThreeClick(event) {
-  allProducts[productThreeIndex].numClicks += 1;
-  allProducts[productOneIndex].numDisplays += 1;
-  allProducts[productTwoIndex].numDisplays += 1;
-  allProducts[productThreeIndex].numDisplays += 1;
-  totalClicks += 1;
-  if (totalClicks === 15) {
-    resultButton.removeAttribute('hidden');
-  }
-
-  console.log(allProducts[productThreeIndex].productName + ' clicked ' + allProducts[productThreeIndex].numClicks + ' times');
-
-  displayThree();
-}
-
+//chart data
 var data = {
   labels: [],
   datasets: [
@@ -174,8 +131,8 @@ function handleResultButtonClick(event) {
   for(var i = 0; i < allProducts.length; i++)
   {
     data.labels[i] = allProducts[i].productName;
-    chartMe.datasets[0].bars[i].value = allProducts[i].numClicks;
     chartMe.datasets[0].bars[i].label = 'Clicked/Displayed';
+    chartMe.datasets[0].bars[i].value = allProducts[i].numClicks;
     chartMe.datasets[1].bars[i].value = allProducts[i].numDisplays;
   }
   chartMe.update();
